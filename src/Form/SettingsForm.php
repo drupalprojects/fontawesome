d5713696430748d8f1f877d4c5220e1a1febf059
Daniel.Moberly <Daniel.Moberly@1160788.no-reply.drupal.org>
2017-11-20 13:52:57 -0700

    Issue #2925109 by Daniel.Moberly: Missing administration page for configuring Font Awesome settings

---
+++ src/Form/SettingsForm.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\fontawesome\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\UrlHelper;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Asset\LibraryDiscovery;
+use Drupal\Core\File\FileSystem;
+
+/**
+ * Defines a form that configures fontawesome settings.
+ */
+class SettingsForm extends ConfigFormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'fontawesome_admin_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'fontawesome.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Get current settings.
+    $fontawesome_config = $this->config('fontawesome.settings');
+
+    $form['fontawesome_use_cdn'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Use external file?'),
+      '#description' => $this->t('Checking this box will cause the Plotly.js javascript file to be loaded externally rather than from the local library file.'),
+      '#default_value' => $fontawesome_config->get('fontawesome_use_cdn'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+
+    // Save the updated settings.
+    $this->config('fontawesome.settings')
+      ->set('fontawesome_use_cdn', $values['fontawesome_use_cdn'])
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
